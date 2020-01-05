5ae8ffef5e39fefe54b8c30e61effc239c93af52
Minira Samadova <samadova.minira@gmail.com>
2020-01-05 11:55:15 +0900

    added calculator component, added grid styling

---
+++ src/components/Calculator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="calculator">
+    <div class="display">hello</div>
+    <div>hello</div>
+    <div>hello</div>
+    <div>hello</div>
+    <div>hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1 / 5;
+  background-color: cornflowerblue;
+}
+</style>
