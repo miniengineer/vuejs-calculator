0840fa47bc2ff4b8d490ab32912da1613fb857bb
Minira Samadova <samadova.minira@gmail.com>
2020-01-05 18:03:22 +0900

    added tests for append method

--- __tests__/example.spec.js
+++ __tests__/example.spec.js
@@ -1,12 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Calculator from '@/components/Calculator.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+describe('Calculator.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Calculator);
+  });
+
+  it('should append clicked number to the display and update the current state', () => {
+    wrapper.vm.append("2");
+    expect(wrapper.vm.current).toBe("2");
+  });
+
+  it('should append clicked number to the existing number in the current display', () => {
+    wrapper.vm.current = "2";
+    wrapper.vm.append("2");
+    expect(wrapper.vm.current).toBe("22");
+  });
+
+  // it('should clear value (set to default empty string) when triggering clear method', () => {
+
+  // });
 })
