856e0b29b89efa4e2096e42dbbb13df956d2bd3e
Minira Samadova <samadova.minira@gmail.com>
2020-01-05 12:08:06 +0900

    added calculator buttons

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="calculator">
-    <div class="display">hello</div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
+    <div class="display">59937987</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class="btn zero">0</div>
+    <div>.</div>
+    <div>=</div>
   </div>
 </template>
 
@@ -22,10 +37,15 @@ export default {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: minmax(50px, auto);
+  font-size: 30px;
 }
 
 .display {
   grid-column: 1 / 5;
   background-color: cornflowerblue;
+}
+
+.zero {
+  grid-column: 1 / 3;
 }
 </style>
