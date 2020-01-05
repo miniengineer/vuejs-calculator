f8ce7f1c71f0d8e60e6053dd7dc478829782a265
Minira Samadova <samadova.minira@gmail.com>
2020-01-05 12:17:23 +0900

    added more styling to the calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,22 +4,22 @@
     <div class="btn">C</div>
     <div class="btn">+/-</div>
     <div class="btn">%</div>
-    <div class="btn">÷</div>
+    <div class="btn operator">÷</div>
     <div class="btn">7</div>
     <div class="btn">8</div>
     <div class="btn">9</div>
-    <div class="btn">x</div>
+    <div class="btn operator">x</div>
     <div class="btn">4</div>
     <div class="btn">5</div>
     <div class="btn">6</div>
-    <div class="btn">-</div>
+    <div class="btn operator">-</div>
     <div class="btn">1</div>
     <div class="btn">2</div>
     <div class="btn">3</div>
-    <div class="btn">+</div>
+    <div class="btn operator">+</div>
     <div class="btn zero">0</div>
     <div class="btn">.</div>
-    <div class="btn">=</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
@@ -43,6 +43,7 @@ export default {
 .display {
   grid-column: 1 / 5;
   background-color: cornflowerblue;
+  color: white;
 }
 
 .zero {
@@ -50,7 +51,12 @@ export default {
 }
 
 .btn {
-  background-color: #eee;
-  border: 1px solid #333;
+  background-color: #F2F2F2;
+  border: 1px solid #999;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
